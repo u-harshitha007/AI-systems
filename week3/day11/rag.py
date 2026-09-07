fceacdfd31c4195496631309b20e0bea321fabcc
u-harshitha007 <harshithauppar203@gmail.com>
2026-09-07 11:37:21 +0530

    Add week 3 RAG project

---
+++ week3/day11/rag.py
@@ -0,0 +1,51 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+
+load_dotenv()
+my_api_key=os.getenv("GROQ_API_KEY")
+
+if not my_api_key:
+    raise ValueError("API key kaha hai bhai")
+
+client=Groq(api_key=my_api_key)
+model="openai/gpt-oss-120b"
+
+
+# step 1
+knowledge_base={
+    "age" : " The age of pratyush is 25 years",
+    "net worth" : "The net worth of pratyush is 2000"
+}
+
+# step 2 retreieval
+def retrieve_info(question):
+    question=question.lower()
+    if "age" in question:
+        return knowledge_base["age"]
+    elif "net worth" in question:
+        return knowledge_base["net worth"]
+    else:
+        return None
+def ask_llm(question):
+    context=retrieve_info(question)
+
+    sys_prompt=f"""answer in one line only. Answer only based on this context. do not hallucinate. Context: {context}"""
+    system_message={
+        "role": "system",
+        "content": sys_prompt
+
+    }
+    message={
+        "role": "user",
+        "content": question
+    }
+    messages=[system_message, message]
+    response=client.chat.completions.create(model=model, messages=messages)
+    answer=response.choices[0].message.content
+    return answer
+
+
+question="what is pratyush's age?"
+print(ask_llm(question))
